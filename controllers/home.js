708a65664e18a8afba63541c18076e2a9cf6bcd0
thuann-vn <thuann@evolableasia.vn>
2018-03-23 10:26:22 +0700

    Update place ship api

--- controllers/home.js
+++ controllers/home.js
@@ -3,7 +3,6 @@
  * Home page.
  */
 exports.index = (req, res) => {
-  console.log(req);
   res.render('home', {
     title: 'Home'
   });
@@ -17,10 +16,10 @@ exports.getInvite = (req, res) => {
   var params = req.body;
 
   //Save to session
-  req.session.rows = params.boardWidth;
-  req.session.cols = params.boardHeight;
+  req.session.boardWidth = params.boardWidth;
+  req.session.boardHeight = params.boardHeight;
   req.session.ships = params.ships;
-  req.session.enemyBoard = generateGameBoard(req.session.rows, req.session.cols);
+  req.session.enemyBoard = generateGameBoard(req.params.boardHeight, req.session.boardWidth);
 
   //Response
   if (params.debug) {
@@ -39,165 +38,168 @@ exports.getPlaceShips = (req, res) => {
   req.session.player1 = params.player1;
   req.session.player2 = params.player2;
 
-  var rows = req.session.rows;
-  var cols = req.session.cols;
+  var boardHeight = parseInt(req.session.boardHeight);
+  var boardWidth = parseInt(req.session.boardWidth);
 
   // get position list can use
   var checkIfBlankPosition = function (startPointX, startPointY, shipType, verticalDirection, gameBoard) {
-    if (!verticalDirection) {
-      switch (shipType) {
-        case 'BB': //Ship 4 pieces
-          if (startPointX + 3 > rows - 1) {
-            return false;
-          }
-
-          if (gameBoard[startPointX][startPointY] == 0 && gameBoard[startPointX + 1][startPointY] == 0 && gameBoard[startPointX + 2][startPointY] == 0 && gameBoard[startPointX + 3][startPointY] == 0) {
-            return [
-              [startPointX, startPointY],
-              [startPointX + 1, startPointY],
-              [startPointX + 2, startPointY],
-              [startPointX + 3, startPointY]
-            ]
-          };
-          break;
-        case 'DD': //Ship 2 pieces
-          if (startPointX + 1 > rows - 1) {
-            return false;
-          }
-          if (gameBoard[startPointX][startPointY] == 0 && gameBoard[startPointX + 1][startPointY] == 0) {
-            return [
-              [startPointX, startPointY],
-              [startPointX + 1, startPointY]
-            ]
-          };
-          break;
-        case 'CA': //Ship 3 pieces
-          if (startPointX + 2 > rows - 1) {
-            return false;
-          }
-          if (gameBoard[startPointX][startPointY] == 0 && gameBoard[startPointX + 1][startPointY] == 0 && gameBoard[startPointX + 2][startPointY] == 0) {
-            return [
-              [startPointX, startPointY],
-              [startPointX + 1, startPointY],
-              [startPointX + 2, startPointY]
-            ]
-          };
-          break;
-        case 'CV': //Ship 5 pieces
-          if (startPointX + 3 > rows - 1 || startPointY - 1 < 0) {
-            return false;
-          }
-
-          if (gameBoard[startPointX][startPointY] == 0 && gameBoard[startPointX + 1][startPointY] == 0 && gameBoard[startPointX + 2][startPointY] == 0 && gameBoard[startPointX + 3][startPointY] == 0 && gameBoard[startPointX + 2][startPointY + 1] == 0) {
-            return [
-              [startPointX, startPointY],
-              [startPointX + 1, startPointY],
-              [startPointX + 2, startPointY],
-              [startPointX + 3, startPointY],
-              [startPointX + 1, startPointY - 1]
-            ]
-          }
-          break;
-        case 'OR': //Ship square
-          if (startPointX + 1 > rows - 1 || startPointY + 1 > cols - 1) {
-            return false;
-          }
-
-          if (gameBoard[startPointX][startPointY] == 0 && gameBoard[startPointX + 1][startPointY] == 0 && gameBoard[startPointX][startPointY + 1] == 0 && gameBoard[startPointX + 1][startPointY + 1] == 0) {
-            return [
-              [startPointX, startPointY],
-              [startPointX + 1, startPointY],
-              [startPointX, startPointY + 1],
-              [startPointX + 1, startPointY + 1]
-            ]
-          };
-          break;
+    try{
+      if (!verticalDirection) {
+        switch (shipType) {
+          case 'BB': //Ship 4 pieces
+            if (startPointX + 3 > boardHeight - 1) {
+              return false;
+            }
+  
+            if (gameBoard[startPointX][startPointY] == 0 && gameBoard[startPointX + 1][startPointY] == 0 && gameBoard[startPointX + 2][startPointY] == 0 && gameBoard[startPointX + 3][startPointY] == 0) {
+              return [
+                [startPointX, startPointY],
+                [startPointX + 1, startPointY],
+                [startPointX + 2, startPointY],
+                [startPointX + 3, startPointY]
+              ]
+            };
+            break;
+          case 'DD': //Ship 2 pieces
+            if (startPointX + 1 > boardHeight - 1) {
+              return false;
+            }
+            if (gameBoard[startPointX][startPointY] == 0 && gameBoard[startPointX + 1][startPointY] == 0) {
+              return [
+                [startPointX, startPointY],
+                [startPointX + 1, startPointY]
+              ]
+            };
+            break;
+  
+          case 'CA': //Ship 3 pieces
+            if (startPointX + 2 > boardHeight - 1) {
+              return false;
+            }
+            if (gameBoard[startPointX][startPointY] == 0 && gameBoard[startPointX + 1][startPointY] == 0 && gameBoard[startPointX + 2][startPointY] == 0) {
+              return [
+                [startPointX, startPointY],
+                [startPointX + 1, startPointY],
+                [startPointX + 2, startPointY]
+              ]
+            };
+            break;
+          case 'CV': //Ship 5 pieces
+            if (startPointX + 3 > boardHeight - 1 || startPointY - 1 < 0) {
+              return false;
+            }
+  
+            if (gameBoard[startPointX][startPointY] == 0 && gameBoard[startPointX + 1][startPointY] == 0 && gameBoard[startPointX + 2][startPointY] == 0 && gameBoard[startPointX + 3][startPointY] == 0 && gameBoard[startPointX + 2][startPointY + 1] == 0) {
+              return [
+                [startPointX, startPointY],
+                [startPointX + 1, startPointY],
+                [startPointX + 2, startPointY],
+                [startPointX + 3, startPointY],
+                [startPointX + 1, startPointY - 1]
+              ]
+            }
+            break;
+          case 'OR': //Ship square
+            if (startPointX + 1 > boardHeight - 1 || startPointY + 1 > boardWidth - 1) {
+              return false;
+            }
+  
+            if (gameBoard[startPointX][startPointY] == 0 && gameBoard[startPointX + 1][startPointY] == 0 && gameBoard[startPointX][startPointY + 1] == 0 && gameBoard[startPointX + 1][startPointY + 1] == 0) {
+              return [
+                [startPointX, startPointY],
+                [startPointX + 1, startPointY],
+                [startPointX, startPointY + 1],
+                [startPointX + 1, startPointY + 1]
+              ]
+            };
+            break;
+        }
+      } else {
+        switch (shipType) {
+          case 'BB': //Ship 4 pieces
+            if (startPointY + 3 > boardWidth - 1) {
+              return false;
+            }
+  
+            if (gameBoard[startPointX][startPointY] == 0 && gameBoard[startPointX][startPointY + 1] == 0 && gameBoard[startPointX][startPointY + 2] == 0 && gameBoard[startPointX][startPointY + 3] == 0) {
+              return [
+                [startPointX, startPointY],
+                [startPointX, startPointY + 1],
+                [startPointX, startPointY + 2],
+                [startPointX, startPointY + 3]
+              ]
+            };
+            break;
+          case 'DD': //Ship 2 pieces
+            if (startPointY + 1 > boardWidth - 1) {
+              return false;
+            }
+            if (gameBoard[startPointX][startPointY] == 0 && gameBoard[startPointX][startPointY + 1] == 0) {
+              return [
+                [startPointX, startPointY],
+                [startPointX, startPointY + 1]
+              ]
+            };
+            break;
+          case 'CA': //Ship 3 pieces
+            if (startPointY + 2 > boardWidth - 1) {
+              return false;
+            }
+            if (gameBoard[startPointX][startPointY] == 0 && gameBoard[startPointX][startPointY + 1] == 0 && gameBoard[startPointX][startPointY + 2] == 0) {
+              return [
+                [startPointX, startPointY],
+                [startPointX, startPointY + 1],
+                [startPointX, startPointY + 2]
+              ]
+            };
+            break;
+          case 'CV': //Ship 5 pieces
+            if (startPointY + 3 > boardWidth - 1 || startPointX - 1 < 0) {
+              return false;
+            }
+  
+            if (gameBoard[startPointX][startPointY] == 0 && gameBoard[startPointX][startPointY + 1] == 0 && gameBoard[startPointX][startPointY + 2] == 0 && gameBoard[startPointX][startPointY + 3] == 0 && gameBoard[startPointX - 1][startPointY] == 0) {
+              return [
+                [startPointX, startPointY],
+                [startPointX, startPointY + 1],
+                [startPointX, startPointY + 2],
+                [startPointX, startPointY + 3],
+                [startPointX - 1, startPointY + 2]
+              ]
+            }
+            break;
+          case 'OR': //Ship square
+            if (startPointX + 1 > boardHeight - 1 || startPointY + 1 > boardWidth - 1) {
+              return false;
+            }
+  
+            if (gameBoard[startPointX][startPointY] == 0 && gameBoard[startPointX][startPointY + 1] == 0 && gameBoard[startPointX + 1][startPointY] == 0 && gameBoard[startPointX + 1][startPointY + 1] == 0) {
+              return [
+                [startPointX, startPointY],
+                [startPointX, startPointY + 1],
+                [startPointX + 1, startPointY],
+                [startPointX + 1, startPointY + 1]
+              ]
+            };
+            break;
+        }
       }
-    } else {
-      switch (shipType) {
-        case 'BB': //Ship 4 pieces
-          if (startPointY + 3 > cols - 1) {
-            return false;
-          }
-
-          if (gameBoard[startPointX][startPointY] == 0 && gameBoard[startPointX][startPointY + 1] == 0 && gameBoard[startPointX][startPointY + 2] == 0 && gameBoard[startPointX][startPointY + 3] == 0) {
-            return [
-              [startPointX, startPointY],
-              [startPointX, startPointY + 1],
-              [startPointX, startPointY + 2],
-              [startPointX, startPointY + 3]
-            ]
-          };
-          break;
-        case 'DD': //Ship 2 pieces
-          if (startPointY + 1 > cols - 1) {
-            return false;
-          }
-          if (gameBoard[startPointX][startPointY] == 0 && gameBoard[startPointX][startPointY + 1] == 0) {
-            return [
-              [startPointX, startPointY],
-              [startPointX, startPointY + 1]
-            ]
-          };
-          break;
-        case 'CA': //Ship 3 pieces
-          if (startPointY + 2 > cols - 1) {
-            return false;
-          }
-          if (gameBoard[startPointX][startPointY] == 0 && gameBoard[startPointX][startPointY + 1] == 0 && gameBoard[startPointX][startPointY + 2] == 0) {
-            return [
-              [startPointX, startPointY],
-              [startPointX, startPointY + 1],
-              [startPointX, startPointY + 2]
-            ]
-          };
-          break;
-        case 'CV': //Ship 5 pieces
-          if (startPointY + 3 > cols - 1 || startPointX - 1 < 0) {
-            return false;
-          }
-
-          if (gameBoard[startPointX][startPointY] == 0 && gameBoard[startPointX][startPointY + 1] == 0 && gameBoard[startPointX][startPointY + 2] == 0 && gameBoard[startPointX][startPointY + 3] == 0 && gameBoard[startPointX - 1][startPointY] == 0) {
-            return [
-              [startPointX, startPointY],
-              [startPointX, startPointY + 1],
-              [startPointX, startPointY + 2],
-              [startPointX, startPointY + 3],
-              [startPointX - 1, startPointY + 2]
-            ]
-          }
-          break;
-        case 'OR': //Ship square
-          if (startPointX + 1 > rows - 1 || startPointY + 1 > cols - 1) {
-            return false;
-          }
-
-          if (gameBoard[startPointX][startPointY] == 0 && gameBoard[startPointX][startPointY + 1] == 0 && gameBoard[startPointX + 1][startPointY] == 0 && gameBoard[startPointX + 1][startPointY + 1] == 0) {
-            return [
-              [startPointX, startPointY],
-              [startPointX, startPointY + 1],
-              [startPointX + 1, startPointY],
-              [startPointX + 1, startPointY + 1]
-            ]
-          };
-          break;
-      }
+    }catch(ex){
+      console.log('Place ship exception',[startPointX, startPointY, shipType, verticalDirection, gameBoard]);
     }
-
     return false;
   }
-
 
   //@param direction: 0:Vertical, 1: Horizon
   var getRandomCoordinate = function (shipType, verticalDirection, gameBoard) {
     var isValid = false;
     while (!isValid) {
       var minX = 0;
-      var maxX = req.session.rows - 1;
+      var maxX = req.session.boardHeight - 1;
       var x = Math.floor(Math.random() * (maxX - minX)) + minX;
 
       var minY = 0;
-      var maxY = req.session.rows - 1;
+      var maxY = req.session.boardWidth - 1;
       var y = Math.floor(Math.random() * (maxY - minY)) + minY;
 
       var position = checkIfBlankPosition(x, y, shipType, verticalDirection, gameBoard);
@@ -208,32 +210,11 @@ exports.getPlaceShips = (req, res) => {
     return [];
   }
 
-  //@param direction: 0:Vertical, 1: Horizon
-  // var getBlankPositions = function (shipType, verticalDirection) {
-  //   var result = [];
-  //   for (x = 0; x < rows; x++) {
-  //     for (y = 0; y < cols; y++) {
-  //       var arrayPositions = checkIfBlankPosition(x, y, shipType, verticalDirection);
-  //       if (arrayPositions != false) {
-  //         result.push(arrayPositions);
-  //       }
-  //     }
-  //   }
-  //   return result;
-  // }
-
-  //Get random position in blanks list
-  // function getRandomPosition(positions) {
-  //   min = 0;
-  //   max = positions.length - 1;
-  //   return positions[Math.floor(Math.random() * (max - min)) + min];
-  // }
-
   var ships = req.session.ships;
   //Arrage ships, return false if can not arrange a ship, return object have gameboard and ships if valid
   var arrangeShips = function (triedTime) {
     var arrangedShips = [];
-    var gameBoard = generateGameBoard(rows, cols);
+    var gameBoard = generateGameBoard(boardHeight, boardWidth);
     //Get position for per ships
     for (var k = 0; k < ships.length; k++) {
       for (var l = 0; l < ships[k].quantity; l++) {
@@ -279,7 +260,8 @@ exports.getPlaceShips = (req, res) => {
   req.session.ourShips = arrangedShips;
 
   //Return response
-  if(params.debug==true){
+  console.log(params.debug);
+  if(params.debug){
     console.log(gameBoard);
     res.status(200).send( {
       ships: arrangedShips,
@@ -376,12 +358,12 @@ exports.getNotify = (req, res) => {
 }
 
 //Generate blank game board
-var generateGameBoard = function (rows, cols) {
+var generateGameBoard = function (boardHeight, boardWidth) {
   //Prepare empty board
   var gameBoard = [];
-  for (i = 0; i < rows; i++) {
+  for (i = 0; i < boardHeight; i++) {
     var col = [];
-    for (j = 0; j < cols; j++) {
+    for (j = 0; j < boardWidth; j++) {
       col.push(0);
     }
     gameBoard.push(col);
